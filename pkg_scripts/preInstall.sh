0043daade9c73dc7ae5463dc14858c1ee788c01f
Naveen Mr <naveen.mr@mphasis.com>
2021-05-17 19:43:46 +0530

    to use openjdk 8

--- pkg_scripts/preInstall.sh
+++ pkg_scripts/preInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/usr/bin/update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-openjdk-amd64/bin/java 1
-/usr/bin/update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
+/usr/bin/update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/bin/java 1
+/usr/bin/update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/bin/java
 
 # check for Java installation
 if type java; then
